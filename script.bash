33e57dd71d5ec2265ddb11f9521cb4e96a62a41f
Barend Erasmus <developersworkspace@gmail.com>
2018-05-10 09:52:16 +0200

    update

--- script.bash
+++ script.bash
@@ -1,14 +1,13 @@
 git filter-branch -f --env-filter '
-WRONG_EMAIL="develoeprsworkspace@gmail.com""
 NEW_NAME="Barend Erasmus"
 NEW_EMAIL="developersworkspace@gmail.com"
 
-if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+if [ "$GIT_COMMITTER_EMAIL" != "$NEW_EMAIL" ]
 then
     export GIT_COMMITTER_NAME="$NEW_NAME"
     export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+if [ "$GIT_AUTHOR_EMAIL" != "$NEW_NAME" ]
 then
     export GIT_AUTHOR_NAME="$NEW_NAME"
     export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
