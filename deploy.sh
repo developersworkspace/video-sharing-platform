6cf62d42a283ff154debba9b1a8ab4f0b1c05d16
developersworkspace@gmail.com <developersworkspace@gmail.com>
2018-05-14 15:01:41 +0200

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+# curl -s https://raw.githubusercontent.com/developersworkspace/video-sharing-platform/master/deploy.sh | bash -s
+
 # Remove Directory
 rm -rf /opt/video-sharing-platform
 
@@ -6,13 +8,20 @@ git clone https://github.com/developersworkspace/video-sharing-platform.git /opt
 
 # Install NPM Packages
 npm install -g gulp
+npm install -g typescript
 npm install --prefix /opt/repositories/video-sharing-platform
 
 # Build
 npm run --prefix /opt/repositories/video-sharing-platform build
 
-# Copy dist
+# Copy
 cp -r /opt/repositories/video-sharing-platform/dist /opt/video-sharing-platform
 
 # Remove Directory
 rm -rf /opt/repositories/video-sharing-platform
+
+# Install NPM Packages
+npm install --prefix /opt/video-sharing-platform
+
+# Run
+pm2 start --name video-sharing-platform /opt/video-sharing-platform/app.js
---
+++ user-interfaces/angular/deploy.sh
@@ -0,0 +1,21 @@
+# curl -s https://raw.githubusercontent.com/developersworkspace/video-sharing-platform/master/deploy.sh | bash -s
+
+# Remove Directory
+rm -rf /opt/video-sharing-platform-ui
+
+# Clone Repository
+git clone https://github.com/developersworkspace/video-sharing-platform.git /opt/repositories/video-sharing-platform
+
+# Install NPM Packages
+npm install -g typescript
+npm install -g @angular/cli
+npm install --prefix /opt/repositories/video-sharing-platform/user-interfaces/angular
+
+# Build
+npm run --prefix /opt/repositories/video-sharing-platform/user-interfaces/angular build
+
+# Copy
+cp -r /opt/repositories/video-sharing-platform/user-interfaces/angular/dist /opt/video-sharing-platform-ui
+
+# Remove Directory
+rm -rf /opt/repositories/video-sharing-platform
